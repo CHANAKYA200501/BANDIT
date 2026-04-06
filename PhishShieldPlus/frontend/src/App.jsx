37f7c8d9956477861134fcc03caf23acfa8793dd
TUSHAR SHARMA <chanakya01@TUSHARs-MacBook-Air.local>
2026-04-06 15:24:41 +0530

    ui/ux modified

--- PhishShieldPlus/frontend/src/App.jsx
+++ PhishShieldPlus/frontend/src/App.jsx
@@ -57,7 +57,6 @@ export default function App() {
           
           <Globe />
           <NeuralShield />
-          <ShadowAvatar />
           <TxFlowGraph />
           
           <OrbitControls autoRotate enableZoom={false} autoRotateSpeed={0.5} />
